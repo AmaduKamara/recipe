9c35259a2a81d2810a0842b02bbe4aa85616a500
Amkam <amaduskamara@gmail.com>
2022-02-12 12:17:05 +0000

    Import searchbar into header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 
 import "./Navbar.css";
+import Searchbar from "./Searchbar";
 
 function Navbar() {
   return (
@@ -9,6 +10,7 @@ function Navbar() {
         <Link to="/" className="brand">
           <h1>Cooking Avenger</h1>
         </Link>
+        <Searchbar />
         <Link to="/create">Create Recipe</Link>
       </nav>
     </div>
