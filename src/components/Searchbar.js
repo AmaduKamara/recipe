3860066603a6fed1ddffd9ac7c02a753d28a24aa
Amkam <amaduskamara@gmail.com>
2022-02-12 12:16:15 +0000

    work on search component and search handler function

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,22 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
-import './Searchbar.css'
+import "./Searchbar.css";
 
 const Searchbar = () => {
-  const [term, setTerm] = useState('')
+  const [term, setTerm] = useState("");
+
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    history.push(`/search?q=${term}`);
+  };
 
   return (
     <div className="searchbar">
-      <form onSubmit={}>
-        <label className="search" htmlFor="search">Search:</label>
-        <input 
-          type="text" value=""
-          id="search" 
+      <form onSubmit={handleSubmit}>
+        <label className="search" htmlFor="search">
+          Search:
+        </label>
+        <input
+          type="text"
+          value=""
+          id="search"
           placeholder="Search a recipe"
+          onChange={(e) => setTerm(e.target.value)}
+          required
         />
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Searchbar
+export default Searchbar;
