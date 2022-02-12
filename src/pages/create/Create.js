d3fc4b4a146bb9ca36ad132ea005380d8389be00
Amkam <amaduskamara@gmail.com>
2022-02-12 09:31:34 +0000

    Add create form, states and handleSubmit

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,8 +1,54 @@
 // styles
+import { useState } from "react";
+
 import "./Create.css";
 
 const Create = () => {
-  return <div>Create</div>;
+  const [title, setTitle] = useState("");
+  const [method, setMethod] = useState("");
+  const [cookingTime, setCookingTime] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(title, method, cookingTime);
+  };
+
+  return (
+    <div className="create">
+      <h2 className="page-title">Add a new Recipe</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Recipe title:</span>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          <span>Recipe method:</span>
+          <textarea
+            onChange={(e) => setMethod(e.target.value)}
+            value={method}
+            required
+          />
+        </label>
+        <label>
+          <span>Cooking time (minutes):</span>
+          <input
+            type="number"
+            onChange={(e) => setCookingTime(e.target.value)}
+            value={cookingTime}
+            required
+          />
+        </label>
+        <button type="submit" className="button">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Create;
