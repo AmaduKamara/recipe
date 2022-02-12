9f48736268b7e4cf6c4ccf9a22e17ab8b7945800
Amkam <amaduskamara@gmail.com>
2022-02-12 12:44:14 +0000

    Work on Search component for fetching searched recipes

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,7 +1,25 @@
+import { useLocation } from "react-router-dom";
 import "./Search.css";
+import { useFetch } from "./../../hooks/useFetch";
+import RecipeList from "./../../components/RecipeList";
 
 const Search = () => {
-  return <div>Search</div>;
+  const queryString = useLocation().search;
+  const queryParams = new URLSearchParams(queryString);
+  const query = queryParams.get("q");
+
+  const url = "http://localhost:3000/recipes?q=" + query;
+
+  const { data, isPending, error } = useFetch(url);
+
+  return (
+    <div>
+      <h2 className="page-title">Recipes including "{query}"</h2>
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="">Loading...</p>}
+      {data && <RecipeList recipes={data} />}
+    </div>
+  );
 };
 
 export default Search;
