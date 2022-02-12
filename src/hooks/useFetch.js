3bca217b1d9fdd5003d84bdd567eedb9e5101420
Amkam <amaduskamara@gmail.com>
2022-02-12 10:35:35 +0000

    Add fetchOtions in fetchData and spread it

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -19,11 +19,14 @@ export const useFetch = (url, method = "GET") => {
   useEffect(() => {
     const controller = new AbortController();
 
-    const fetchData = async () => {
+    const fetchData = async (fetchOptions) => {
       setIsPending(true);
 
       try {
-        const res = await fetch(url, { signal: controller.signal });
+        const res = await fetch(url, {
+          ...fetchOptions,
+          signal: controller.signal,
+        });
         if (!res.ok) {
           throw new Error(res.statusText);
         }
@@ -42,12 +45,17 @@ export const useFetch = (url, method = "GET") => {
       }
     };
 
-    fetchData();
+    if (method === "GET") {
+      fetchData();
+    }
+    if (method === "POST" && options) {
+      fetchData(options);
+    }
 
     return () => {
       controller.abort();
     };
-  }, [url]);
+  }, [url, options, method]);
 
   return { data, isPending, error, postData };
 };
