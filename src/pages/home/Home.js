382f48b2c5d136a522a7882141cef19e3a03d411
Amkam <amaduskamara@gmail.com>
2022-02-13 13:34:06 +0000

    Edit firebase/firestore import in firebase config file

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,28 @@
-import { useFetch } from "../../hooks/useFetch";
+import { useEffect, useState } from "react";
+// import { useFetch } from "../../hooks/useFetch";
+import { projectFirestore } from "../../firebase/config";
+
 import RecipeList from "../../components/RecipeList";
 
 import "./Home.css";
 
 const Home = () => {
-  const { data, isPending, error } = useFetch("http://localhost:3000/recipes");
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(false);
+
+  // Used for json-server data
+  // const { data, isPending, error } = useFetch("http://localhost:3000/recipes");
+  useEffect(() => {
+    setIsPending(true);
+
+    projectFirestore
+      .collection("recipes")
+      .get()
+      .then((snapshot) => {
+        console.log(snapshot);
+      });
+  }, []);
 
   return (
     <div className="home">
