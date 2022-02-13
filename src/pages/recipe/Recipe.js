67d91380a5d19ae05dceffd9c0e348e5767d9cc9
Amkam <amaduskamara@gmail.com>
2022-02-13 11:32:38 +0000

    Add dark and light mode styles for Recipe

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -3,14 +3,16 @@ import { useParams } from "react-router-dom";
 // Styles
 import "./Recipe.css";
 import { useFetch } from "./../../hooks/useFetch";
+import { useTheme } from "./../../hooks/useTheme";
 
 const Recipe = () => {
   const { id } = useParams();
   const url = "http://localhost:3000/recipes/" + id;
   const { data: recipe, isPending, error } = useFetch(url);
+  const { mode } = useTheme();
 
   return (
-    <div className="recipe">
+    <div className={`recipe ${mode}`}>
       {error && <p>{error}</p>}
       {isPending && <p>Loading...</p>}
       {recipe && (
