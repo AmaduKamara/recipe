a73ec969ab9ae71f28f0eb634bb3df2f179c60d8
Amkam <amaduskamara@gmail.com>
2022-02-13 11:11:59 +0000

    make use of the changeMode function in themea selector component

--- src/components/ThemeSelector.jsx
+++ src/components/ThemeSelector.jsx
@@ -3,14 +3,24 @@ import React from "react";
 import { useTheme } from "../hooks/useTheme";
 
 import "./ThemeSelector.css";
+import modeIcon from "../assets/mode-icon.svg";
 
 const themeColors = ["teal", "#58249c", "#249c6b", "orange", "#b70233"];
 
 const ThemeSelector = () => {
-  const { changeColor } = useTheme();
+  const { changeColor, changeMode, mode } = useTheme();
+
+  const toggleMode = () => {
+    changeMode(mode === "dark" ? "light" : "dark");
+  };
+
+  console.log(mode);
 
   return (
     <div className="theme-selector">
+      <div className="mode-toggle">
+        <img src={modeIcon} alt="Mode icon" onClick={toggleMode} />
+      </div>
       <div className="theme-buttons">
         {themeColors.map((color) => (
           <div
