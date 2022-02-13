dd839cd386fe73f9850812d325a9c61f73e8fcdb
Amkam <amaduskamara@gmail.com>
2022-02-13 11:28:52 +0000

    Add dark and light mode styles for RecipeList

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -2,14 +2,19 @@
 import { Link } from "react-router-dom";
 import "./RecipeList.css";
 
+import { useTheme } from "../hooks/useTheme";
+
 const RecipeList = ({ recipes }) => {
+  const { mode } = useTheme();
+
   if (recipes.length === 0) {
     return <div className="error">No recipes to load...</div>;
   }
+
   return (
     <div className="recipe-list">
       {recipes.map((recipe) => (
-        <div key={recipe.id} className="card">
+        <div key={recipe.id} className={`card ${mode}`}>
           <h3>{recipe.title}</h3>
           <p>{recipe.cookingTime} to make</p>
           <div>{recipe.method.substring(0, 100)}...</div>
