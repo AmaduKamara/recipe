4447a174046d9f431bc7338116e1c7b94cb1dc48
Amkam <amaduskamara@gmail.com>
2022-02-13 10:01:59 +0000

    Use the useThem to get the context value

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,11 @@
 import { Link } from "react-router-dom";
-import { useContext } from "react";
-import { ThemeContext } from "../context/ThemeContext";
+import { useTheme } from "../hooks/useTheme";
 
 import "./Navbar.css";
 import Searchbar from "./Searchbar";
 
 function Navbar() {
-  const { color } = useContext(ThemeContext);
+  const { color } = useTheme();
 
   return (
     <div className="navbar" style={{ background: color }}>
